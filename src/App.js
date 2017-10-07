8e3cc5a2dd7c325a5c1c51bdb3c1c1c611768c5a
Mihajlo Lazar <mihajlolazar@hotmail.com>
2017-10-07 19:05:26 +0200

    added autFocus to the search input field

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ class BooksApp extends React.Component {
                     However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                     you don't find a specific author or title. Every search is limited by search terms.
                   */}
-                  <input type="text" placeholder="Search by title or author" defaultValue={this.state.searchQuery} onChange={(event) => this.searchBook(event)}/>
+                  <input type="text" autoFocus={true} placeholder="Search by title or author" defaultValue={this.state.searchQuery} onChange={(event) => this.searchBook(event)}/>
                 </div>
               </div>
                 {this.state.searchBooks.length && (
